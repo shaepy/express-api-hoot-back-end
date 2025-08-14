abd6c8c24a388a808ca3c315f10432def423f7b1
shaexintran <shaexin@icloud.com>
2025-08-14 12:23:45 -0700

    post - create comment working

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -13,10 +13,27 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
+// NEW COMMENT - POST - /hoots/:hootId/comments
+router.post("/:hootId/comments", verifyToken, async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+    // comments is embedded
+    hoot.comments.push({ ...req.body, author: req.user._id });
+    await hoot.save();
+    const newComment = hoot.comments[hoot.comments.length - 1];
+    console.log("newComment is:", newComment);
+    res.status(201).json(newComment);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
 // VIEW HOOTS - GET - /hoots
 router.get("/", verifyToken, async (req, res) => {
   try {
-    const hoots = await Hoot.find({}).populate("author").sort({ createdAt: "desc" });
+    const hoots = await Hoot.find({})
+      .populate("author")
+      .sort({ createdAt: "desc" });
     res.status(200).json(hoots);
   } catch (err) {
     res.status(500).json({ err: err.message });
@@ -26,7 +43,10 @@ router.get("/", verifyToken, async (req, res) => {
 // VIEW HOOT - GET - /hoots/:hootId
 router.get("/:hootId", verifyToken, async (req, res) => {
   try {
-    const hoot = await Hoot.findById(req.params.hootId).populate("author");
+    const hoot = await Hoot.findById(req.params.hootId).populate(
+      "author",
+      "comments.author"
+    );
     res.status(200).json(hoot);
   } catch (err) {
     res.status(500).json({ err: err.message });
@@ -36,18 +56,37 @@ router.get("/:hootId", verifyToken, async (req, res) => {
 // UPDATE HOOT - PUT - /hoots/:hootId
 router.put("/:hootId", verifyToken, async (req, res) => {
   try {
-    const hoot = await Hoot.findById(req.params.hootId).populate("author");
+    const hoot = await Hoot.findById(req.params.hootId);
+    console.log("Found the hoot to update:", hoot);
+
     // compare to see if req.user is the author of this hoot
-    if (req.user._id !== hoot.author._id) {
+    if (req.user._id !== hoot.author.toString()) {
       return res.status(403).send("Unauthorized access");
     }
     const updatedHoot = await Hoot.findByIdAndUpdate(
       req.params.hootId,
-      { ...req.body, author: req.user._id },
+      req.body,
       { new: true }
     );
     console.log("Updated Hoot is:", updatedHoot);
     res.status(200).json(updatedHoot);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// DELETE HOOT - DELETE - /hoots/:hootId
+router.delete("/:hootId", verifyToken, async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+    console.log("Found the hoot to update:", hoot);
+
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send("Unauthorized access");
+    }
+
+    const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
+    res.status(200).json(deletedHoot);
   } catch (err) {
     res.status(500).json({ err: err.message });
   }
